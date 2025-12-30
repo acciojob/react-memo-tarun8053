505ea7e7a17a4ba80b38fbda0f80b33c106eb1c7
Tarun <thakurtarun1012@gmail.com>
2025-12-30 14:00:40 +0530

    Update ReactMemo.js

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 
-const ReactMemo = React.memo(({ todos, addCustomTodo }) => {
+const ReactMemo = React.memo(({ todos, addSkill }) => {
   const [skill, setSkill] = useState("");
 
   return (
     <div id="react-memo">
       <h3>Todo List (React.memo)</h3>
 
-      {/* REQUIRED FOR CYPRESS */}
+      {/* 🔴 INPUT MUST BE DIRECTLY HERE */}
       <input
         type="text"
         value={skill}
         onChange={(e) => setSkill(e.target.value)}
       />
 
+      {/* 🔴 BUTTON MUST BE DIRECTLY HERE */}
       <button
         onClick={() => {
-          addCustomTodo(skill);
+          addSkill(skill);
           setSkill("");
         }}
       >
