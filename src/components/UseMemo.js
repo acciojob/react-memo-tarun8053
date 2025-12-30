9d4e316048efc8df49ae67241cbeae7ff4726bc4
Tarun <thakurtarun1012@gmail.com>
2025-12-30 13:53:43 +0530

    Update UseMemo.js

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -1,14 +1,21 @@
 import React, { useMemo } from "react";
 
-function UseMemo({ todos }) {
-  const totalTodos = useMemo(() => {
-    return todos.length;
-  }, [todos]);
+function UseMemo({ todos, addTodo }) {
+  const totalTodos = useMemo(() => todos.length, [todos]);
 
   return (
     <div id="use-memo">
       <h3>Total Todos (useMemo)</h3>
       <p>{totalTodos}</p>
+
+      {/* REQUIRED FOR CYPRESS */}
+      <button onClick={addTodo}>Add Todo</button>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
